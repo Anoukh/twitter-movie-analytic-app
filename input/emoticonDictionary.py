742e302adc09c42849ec2ff206ce46466c172e8a
anoukh <anoukh.ashley@gmail.com>
2017-06-11 07:13:57 +0530

    Integrated NER. Code Cleanup. Commenting Done

--- input/emoticonDictionary.py
+++ input/emoticonDictionary.py
@@ -2,6 +2,16 @@ def select_emoticon(x):
     return {
         ":)": 'happy',
         ":(": 'sad',
-        ":o": 'surprised'
-
+        ":o": 'surprised',
+        ":/": 'confused',
+        "gr8": 'great',
+        "n8": 'night',
+        "gd": 'good',
+        "awsm": 'awesome',
+        "h8": 'hate',
+        "lol": "laugh",
+        "rofl": "laugh",
+        "rotfl": "laugh",
+        "srsly": "seriously",
+        "thx": "thanks"
     }.get(x, x)
--- input/nltkPreProcessing.py
+++ input/nltkPreProcessing.py
@@ -1,5 +1,4 @@
 import json
-# import csv
 from nltk.tokenize.casual import TweetTokenizer
 from nltk.corpus import stopwords
 from nltk import pos_tag
@@ -11,46 +10,54 @@ import re
 import string
 import en_core_web_md
 
-trackedHashTag = "dearzindagi"
+actor = ['hugh', 'jackman', 'patrick', 'stewart', 'wolverine', 'dafne', 'keen']
+director = ['james', 'mangold']
+
 
 def main():
-    nlp = en_core_web_md.load()
+    # Load Scipy Model for Entity Recognition
+    # nlp = en_core_web_md.load()
 
+    # Output file
     output_file = open('/Users/anoukh/FYP/Datasets/Yashoda/LoganPreProcessed.csv', "wb")
     writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, escapechar=',',
                         encoding="utf-8")
     lemmatizer = WordNetLemmatizer()
     count = 0
     output_twitter_array = []
+
     # Append Headings
     output_twitter_array.append("date")
     output_twitter_array.append("text")
-    # output_twitter_array.append("lat")
-    # output_twitter_array.append("long")
-    # output_twitter_array.append("ner")
+    output_twitter_array.append("lat")
+    output_twitter_array.append("long")
+    output_twitter_array.append("ner")
     output_twitter_array.append("total")
-
+    writer.writerow(output_twitter_array)
     # End Append Headings
-    # writer.writerow(output_twitter_array)
 
     twitter_array = []
     counter = 0
+
+    # Load JSON Object to Python Array
     for line in open('/Users/anoukh/FYP/Datasets/Logan/logan.json', 'r'):
         line = json.loads(line)
-        twitter_array.append([line["text"], line["created_at"], line["coordinates"]])
-        counter += 1
-    print counter
-
+        twitter_array.append([line["text"], line["created_at"], line["coordinates"], line['favorite_count',
+                                                                                          line['retweet_count']]])
     print len(twitter_array)
+
+    # Remove duplicate tweets
     unique_tweet_set = dict((x[0], x[1:]) for x in twitter_array)
     print len(unique_tweet_set)
     count1 = 0
     for key in unique_tweet_set:
         output_twitter_array = []
         date_coordinate_object = unique_tweet_set[key]
-        if(True):  #Only process tweets with coordinates
+
+        # Only process tweets with coordinates
+        if date_coordinate_object[1] != None:
             print "Cordinates"
-            count1 = count1 + 1
+            count1 += 1
             print count1
 
             # Tokenize
@@ -75,36 +82,38 @@ def main():
 
             # Remove Stop Words and Punctuations
             stopwords_punctuations = stopwords.words('english') + list(string.punctuation)
-            new_tokenized_tweets = [word for word in tokenized_tweets if word.lower() not in stopwords_punctuations]
+            new_tokenized_tweets = [word for word in lemmatized_sentence if word.lower() not in stopwords_punctuations]
 
+            # Print Date, Retweet Count, Favorite Count
             output_twitter_array.append(date_coordinate_object[0])
-            # output_twitter_array.append(replace_unnecessary_tokens(new_tokenized_tweets))
+            output_twitter_array.append(date_coordinate_object[2])
+            output_twitter_array.append(date_coordinate_object[3])
             try:
                 find_entity = " ".join(map(str, replace_unnecessary_tokens(new_tokenized_tweets)))
                 output_twitter_array.append(find_entity)
             except UnicodeEncodeError:
                 continue
 
-            # try:
-            #     output_twitter_array.append(date_coordinate_object[1]['coordinates'][0])
-            #     output_twitter_array.append(date_coordinate_object[1]['coordinates'][1])
-            # except TypeError:
-            #     output_twitter_array.append(0.0)
-            #     output_twitter_array.append(0.0)
-            # except KeyError:
-            #     output_twitter_array.append(0.0)
-            #     output_twitter_array.append(0.0)
+            try:
+                output_twitter_array.append(date_coordinate_object[1]['coordinates'][0])
+                output_twitter_array.append(date_coordinate_object[1]['coordinates'][1])
+            except TypeError:
+                output_twitter_array.append(0.0)
+                output_twitter_array.append(0.0)
+            except KeyError:
+                output_twitter_array.append(0.0)
+                output_twitter_array.append(0.0)
 
             # Finding Entities
 
-            entity = []
-
+            # entity = []
             # doc = nlp(unicode(find_entity))
             # for temp in doc:
             #     if temp.ent_type_ != "":
             #         entity.append(temp.ent_type_)
 
-            # output_twitter_array.append(entity)
+            entity = get_entity(find_entity)
+            output_twitter_array.append(entity)
             output_twitter_array.append(key)
             writer.writerow(output_twitter_array)
             # print output_twitter_array
@@ -121,7 +130,7 @@ def main():
 # TODO: Detect Outliers
 def replace_unnecessary_tokens(tokens):
     i = 0
-    newTokens = []
+    new_tokens = []
     flag = 'false'
     hyphen_pattern = re.compile(r'\w+(?:-\w+)+')
     url_pattern = rtext = re.compile(r'^https?:\/\/.*[\r\n]*')
@@ -131,12 +140,12 @@ def replace_unnecessary_tokens(tokens):
                 if tokens[index] == u'\u2026':
                     continue
                 elif tokens[index] == u'\u2764':
-                    newTokens.append("love")
+                    new_tokens.append("love")
                     flag = 'true'
                 elif tokens[index] == u'\ud83d':
                     emojiWord = emojiDictionary.select_emoji(tokens[index+1])
                     if emojiWord != 'unknown':
-                        newTokens.append(emojiWord)
+                        new_tokens.append(emojiWord)
                     flag = 'true'
                 # Remove Hashtags and Mentions
                 elif tokens[index][:1] == '#' or tokens[index][:1] == '@':
@@ -145,7 +154,7 @@ def replace_unnecessary_tokens(tokens):
                 elif hyphen_pattern.match(tokens[index]):
                     word = tokens[index].split('-')
                     for w in word:
-                        newTokens.append(w)
+                        new_tokens.append(w)
                 # URL removal
                 elif url_pattern.match(tokens[index]):
                     continue
@@ -162,18 +171,30 @@ def replace_unnecessary_tokens(tokens):
                 elif tokens[index]:
                     try:
                         str(tokens[index])
-                        newTokens.append(emoticonDictionary.select_emoticon(tokens[index]))
+                        new_tokens.append(emoticonDictionary.select_emoticon(tokens[index]))
                     except Exception:
                         continue
                 else:
-                    newTokens.append(emoticonDictionary.select_emoticon(tokens[index]))
+                    new_tokens.append(emoticonDictionary.select_emoticon(tokens[index]))
                 i += 1
             except IndexError:
                 print "Error"
         else:
             flag = 'false'
             continue
-    return newTokens
+    return new_tokens
+
+
+def get_entity(text):
+    entity_list = []
+    text = map(lambda x: x.lower(), text.split())
+    if any(st.lower() in text for st in actor) or 'act' in text:
+        entity_list.append('ACTOR')
+    if any(st in text for st in director):
+        entity_list.append('DIR')
+    if 'song' in text or 'soundtrack' in text:
+        entity_list.append('SONG')
+    return entity_list
 
 
 if __name__ == '__main__':
